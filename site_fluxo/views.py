6ea6839c21bdb2126a00e6cfef783ff69ffcc22a
ItaloGui98 <italoguilherme974@gmail.com>
2026-09-16 16:05:49 -0300

    criando views e urls

--- site_fluxo/views.py
+++ site_fluxo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'site_fluxo/home.html')
+
+def portifolio(request):
+    return render(request, 'site_fluxo/portifolio.html')
+
